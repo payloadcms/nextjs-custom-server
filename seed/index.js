93b121b929dcd9e352ce2db39b4256be68e00e53
James <james@trbl.design>
2021-02-05 16:39:12 -0500

    feat: seed simplifications

--- seed/index.js
+++ seed/index.js
@@ -1,14 +1,12 @@
 const payload = require('payload');
 const path = require('path');
-const fs = require('fs');
-const {Readable} = require('stream');
 
 const home = require('./home.json');
 const sample = require('./sample.json');
 
 require('dotenv').config();
 
-const {PAYLOAD_SECRET_KEY, MONGO_URL} = process.env;
+const { PAYLOAD_SECRET_KEY, MONGO_URL } = process.env;
 
 payload.init({
   secret: PAYLOAD_SECRET_KEY,
@@ -16,59 +14,30 @@ payload.init({
   local: true,
 });
 
-/**
- * Save buffer data to a file.
- * @param {Buffer} buffer - buffer to save to a file.
- * @param {string} filePath - path to a file.
- */
-const saveBufferToFile = async (buffer, filePath) => {
-  // Setup readable stream from buffer.
-  let streamData = buffer;
-  let readStream = Readable();
-  readStream._read = () => {
-    readStream.push(streamData);
-    streamData = null;
-  };
-  // Setup file system writable stream.
-  return fs.writeFileSync(filePath, buffer);
-};
-
 const createHomePage = async () => {
-  const fileRead = fs.readFileSync(path.resolve(__dirname, './payload.jpg'));
-  const file = {
-    name: 'payload.jpg',
-    data: fileRead,
-    mimetype: 'image/jpeg',
-    mv: async (filePath) => {
-      return saveBufferToFile(fileRead, filePath);
-    }
-  };
-
   const createdMedia = await payload.create({
     collection: 'media',
     data: {
       alt: 'Payload',
-      name: 'Payload',
-      filename: 'payload.jpg',
     },
-    file,
+    filePath: path.resolve(__dirname, './payload.jpg'),
   });
 
   const createdSamplePage = await payload.create({
     collection: 'pages',
-    data: sample
+    data: sample,
   });
 
   const homeString = JSON.stringify(home)
     .replaceAll('{{IMAGE_ID}}', createdMedia.id)
     .replaceAll('{{SAMPLE_PAGE_ID}}', createdSamplePage.id);
 
-  const createdPage = await payload.create({
+  await payload.create({
     collection: 'pages',
     data: JSON.parse(homeString),
   });
 
-  console.log('Seed completed!')
+  console.log('Seed completed!');
   process.exit(0);
 };
 
