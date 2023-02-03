cbdb2200d96207b06b0cf92176799857b3243105
Jessica Boezwinkle <jessica@trbl.design>
2023-02-03 17:22:57 +0000

    fix: updates seed and readme

--- readme.md
+++ readme.md
@@ -35,9 +35,9 @@ Here is a step-by-step guide for how to use this repo:
 1. Clone this repo using `git clone --depth=1 https://github.com/payloadcms/nextjs-custom-server.git <YOUR_PROJECT_NAME>`
 1. Run `cp .env.example .env` to create an `.env` file
 1. Fill out your `.env` file with values that describe your environment
+1. *optional* Set `SEED_DATABASE=true` in your `.env` to add sample pages and a Media upload (set this to false after initial seed)
 1. Run `yarn` or `npm install`
 1. Run `yarn dev` to open a development environment
-1. *optional* Run `yarn seed` to add sample pages and a Media upload
 1. Go to [http://localhost:3000/admin](http://localhost:3000/admin) to create your first user
 
 ## Building and serving in Production
--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,8 @@ import nextBuild from 'next/dist/build';
 import express from 'express';
 import payload from 'payload';
 import { config as dotenv } from 'dotenv';
+import { home } from '../seed/home';
+import { sample } from '../seed/sample';
 
 dotenv({
   path: path.resolve(__dirname, '../.env'),
@@ -17,6 +19,33 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     mongoURL: process.env.MONGODB_URI,
     express: server,
+    onInit: async () => {
+      if (process.env.SEED_DATABASE === 'true') {
+        const { id: createdMedia } = await payload.create({
+          collection: 'media',
+          data: {
+            alt: 'Payload',
+          },
+          filePath: path.resolve(__dirname, './payload.jpg'),
+        });
+
+        const { id: createdSamplePage } = await payload.create({
+          collection: 'pages',
+          data: sample,
+        });
+
+        const homeString = JSON.stringify(home)
+          .replace(/{{IMAGE_ID}}/g, createdMedia)
+          .replace(/{{SAMPLE_PAGE_ID}}/g, createdSamplePage);
+
+        payload.create({
+          collection: 'pages',
+          data: JSON.parse(homeString),
+        });
+
+        console.log('Seed completed!');
+      }
+    },
   });
 
   if (!process.env.NEXT_BUILD) {
