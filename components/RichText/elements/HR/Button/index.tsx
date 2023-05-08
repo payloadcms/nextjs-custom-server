0c0f78c5afb7a3859e61b1863a7dfc8cc70fc37a
neo1415 <adedaniel502@gmail.com>
2023-05-08 14:55:47 +0100

    ap

--- components/RichText/elements/HR/Button/index.tsx
+++ components/RichText/elements/HR/Button/index.tsx
@@ -1,51 +1,51 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React, { useCallback } from 'react';
-import { Transforms } from 'slate';
-import { useSlate, ReactEditor } from 'slate-react';
-import { ElementButton } from 'payload/components/rich-text';
+// /* eslint-disable import/no-extraneous-dependencies */
+// import React, { useCallback } from 'react';
+// import { Transforms } from 'slate';
+// import { useSlate, ReactEditor } from 'slate-react';
+// import { ElementButton } from 'payload/components/rich-text';
 
-const baseClass = 'rich-text-hr-button';
+// const baseClass = 'rich-text-hr-button';
 
-const insertHR = (editor) => {
-  const text = { text: ' ' };
-  const hr = {
-    type: 'hr',
-    children: [
-      text,
-    ],
-  };
+// const insertHR = (editor) => {
+//   const text = { text: ' ' };
+//   const hr = {
+//     type: 'hr',
+//     children: [
+//       text,
+//     ],
+//   };
 
-  const nodes = [hr, { children: [{ text: '' }] }];
+//   const nodes = [hr, { children: [{ text: '' }] }];
 
-  if (editor.blurSelection) {
-    Transforms.select(editor, editor.blurSelection);
-  }
+//   if (editor.blurSelection) {
+//     Transforms.select(editor, editor.blurSelection);
+//   }
 
-  Transforms.insertNodes(editor, nodes);
+//   Transforms.insertNodes(editor, nodes);
 
-  const currentPath = editor.selection.anchor.path[0];
-  const newSelection = { anchor: { path: [currentPath + 1, 0], offset: 0 }, focus: { path: [currentPath + 1, 0], offset: 0 } };
+//   const currentPath = editor.selection.anchor.path[0];
+//   const newSelection = { anchor: { path: [currentPath + 1, 0], offset: 0 }, focus: { path: [currentPath + 1, 0], offset: 0 } };
 
-  Transforms.select(editor, newSelection);
-  ReactEditor.focus(editor);
-};
+//   Transforms.select(editor, newSelection);
+//   ReactEditor.focus(editor);
+// };
 
-const ToolbarButton: React.FC<{ path: string }> = () => {
-  const editor = useSlate();
+// const ToolbarButton: React.FC<{ path: string }> = () => {
+//   const editor = useSlate();
 
-  const handleAddHR = useCallback(() => {
-    insertHR(editor);
-  }, [editor]);
+//   const handleAddHR = useCallback(() => {
+//     insertHR(editor);
+//   }, [editor]);
 
-  return (
-    <ElementButton
-      className={baseClass}
-      format="hr"
-      onClick={handleAddHR}
-    >
-      —
-    </ElementButton>
-  );
-};
+//   return (
+//     <ElementButton
+//       className={baseClass}
+//       format="hr"
+//       onClick={handleAddHR}
+//     >
+//       —
+//     </ElementButton>
+//   );
+// };
 
-export default ToolbarButton;
+// export default ToolbarButton;
